009a1fd8374a6ba075811b63191677095fd8173b
Adam Rogas <adam@rogassi.com>
2020-12-01 13:55:01 -0800

    check node version

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as core from '@actions/core';
 import {getInputs, Inputs} from './context';
 import * as docker from './docker';
 import * as stateHelper from './state-helper';
-
+import AWS, {ECR, Config, Credentials} from 'aws-sdk';
 export async function run(): Promise<void> {
   try {
     if (os.platform() !== 'linux') {
@@ -11,6 +11,21 @@ export async function run(): Promise<void> {
     }
 
     const {registry, username, password, logout} = getInputs();
+
+    const creds = new Credentials({
+      accessKeyId: username,
+      secretAccessKey: password
+    });
+    AWS.config.credentials = creds;
+
+    var ecr = new ECR();
+    var token = await ecr.getAuthorizationToken({registryIds: [registry]}).promise();
+    var data = token.authorizationData;
+    if (data) {
+      core.info(`🔑 Getting Token...`)
+      core.setOutput('token', data[0].authorizationToken);
+    }
+    return;
     stateHelper.setRegistry(registry);
     stateHelper.setLogout(logout);
     await docker.login(registry, username, password);
